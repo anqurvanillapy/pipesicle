93a1bf2d18ae95af9774237d08e0e09b3e5cf0a7
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-10-27 15:20:18 +0800

    updated abstract class

--- metassg/core.py
+++ metassg/core.py
@@ -1,8 +1,32 @@
 # -*- coding: utf-8 -*-
 
-import os
+from abc import abstractmethod, ABCMeta
 
 
-class MetaSSG(object):
+class MetaSSG(metaclass=ABCMeta):
     """SSG base class"""
-    pass
+    @abstractmethod
+    def clean(self):
+        """Clean all legacy generated pages"""
+        pass
+
+    def load_templates(self, fpath):
+        """\
+        - Load templates using Jinja2 Environment
+            + `fpath` can be filename and directory
+            + FileSystemLoader is specified
+        """
+        pass
+
+    @abstractmethod
+    def render(self, posts):
+        """\
+        - Render the posts
+            + Using Jinja2 render the templates
+            + Publish pages into the destination directory
+        """
+        pass
+
+    def send_static(self):
+        """Send static assets to destination directory"""
+        pass
